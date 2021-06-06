803bcc09b08e3fbe90e41505ebd104069b638ff8
farhanfitrahtur <farhanfitrahtur@gmail.com>
2021-06-06 21:31:59 +0700

    add admin layouts

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Admin Pages
+
+Route::get('/admin', function () {
+    return view('admin.layouts.admin');
+});
+
+
+
+// Route::prefix('/jobsaya')->group(function () {
+//     Route::get('/', [KerjaController::class, 'myjob'])->name('user.jobsaya');
+//     Route::post('/store', [KerjaController::class, 'store']);
+//     Route::get('/get/{id}', [KerjaController::class, 'getById']);
+//     Route::delete('/delete/{id}',[KerjaController::class, 'delete']);
+//     Route::put('/update/{id}', [KerjaController::class, 'update']);
+// });
+
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
